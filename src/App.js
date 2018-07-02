c8bcda19ac32fac93872a436f36f82c9b6986349
ecoologic <erikecoologic@gmail.com>
2018-07-02 21:35:48 +0200

    wip timer

--- src/App.js
+++ src/App.js
@@ -4,22 +4,18 @@ import './App.css';
 
 import Exerciser from './Exerciser'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
 
-        <Exerciser />
-      </div>
-    );
-  }
-}
+    <Exerciser />
+  </div>
+)
 
 export default App;
--- src/Exerciser.jsx
+++ src/Exerciser.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import MultiplicationExercise from './MultiplicationExercise'
 
-const Timer = () => { return "Timer" }
+const Timer = (props) => <div>Time Left: {props.seconds} Seconds</div>
 const Counter = (props) => <div>Points: {props.points}</div>
 const Log = () => { return "Log" }
 
@@ -9,7 +9,7 @@ export default class Exerciser extends Component {
   constructor() {
     super()
     this.state = {
-      time: 60,
+      seconds: 60,
       points: 0
     }
 
@@ -22,7 +22,7 @@ export default class Exerciser extends Component {
   render() {
     return (
       <div>
-        <Timer />
+        <Timer seconds={this.state.seconds} />
         <MultiplicationExercise onScore={this.onScore} />
         <Counter points={this.state.points} />
         <Log />
